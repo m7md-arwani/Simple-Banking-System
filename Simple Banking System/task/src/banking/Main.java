4bee8959d589a58803f7e3d50792fa763d3eb766
m7md-arwani <moh.3ara@hotmail.com>
2021-09-11 17:49:00 +0300

    Functionalities that have been added. Create New account Log in and out Show balance. (Stage 1)

--- Simple Banking System/task/src/banking/Main.java
+++ Simple Banking System/task/src/banking/Main.java
@@ -1,7 +1,58 @@
 package banking;
 
+import java.util.*;
+
 public class Main {
+    public static Scanner scanner = new Scanner(System.in);
+    // key = account number, value = list of pin and balance
+    public static Map<String, List<Integer>> accounts = new HashMap<>();
+    public static List<Integer> data = new ArrayList<>(); // current opened accounts
+
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        state.start();
+
+
     }
+
+    public static void creteNewAccount() {
+        Random random = new Random();
+        String bin = "400000";
+        int n = 1000000000 + random.nextInt(9000000);
+        String last10Digit = Integer.toString(n);
+        String accountNumber = bin + last10Digit;
+        int pin = 1000 + random.nextInt(9000);
+
+        System.out.println("Your card has been created");
+        System.out.println("Your card number:");
+        System.out.println(accountNumber);
+        System.out.println("Your card PIN:");
+        System.out.println(pin);
+        // Storing the data of the new account in a hashmap.
+        accounts.put(accountNumber, new ArrayList<>(List.of(pin, 0)));
+
+
+    }
+
+    public static void login() {
+        System.out.println("Enter your card number:");
+        String card = scanner.nextLine();
+        System.out.println("Enter your PIN:");
+        int pin = scanner.nextInt();
+        scanner.nextLine();
+
+        if (accounts.get(card) == null) {
+            System.out.println("Account number does not exist");
+        } else {
+            data.addAll(accounts.get(card));
+            if (pin == data.get(0)) {
+                System.out.println("You have successfully logged in!");
+                state.currentState = state.State.LOGGED_IN;
+            }
+        }
+    }
+
+
 }
+
+
+
---
+++ Simple Banking System/task/src/banking/state.java
@@ -0,0 +1,76 @@
+package banking;
+
+import java.util.Scanner;
+
+public class state {
+    public static Scanner scanner = new Scanner(System.in);
+    public static boolean isRunning = true;
+    public static State currentState = State.SHOW_MENU;
+
+    enum State {
+        CREATE_AN_ACCOUNT, LOG_INTO_ACCOUNT, SHOW_MENU, LOGGED_IN
+    }
+
+    public static void start() {
+        while (isRunning) {
+            operationManager(currentState);
+        }
+    }
+
+    public static void operationManager(State state) {
+        switch (state) {
+            case SHOW_MENU:
+                System.out.println("1. Create an account\n2. Log into account\n0. Exit");
+                int choice = scanner.nextInt();
+                scanner.nextLine();
+                switch (choice) {
+                    case 1:
+                        currentState = State.CREATE_AN_ACCOUNT;
+                        break;
+                    case 2:
+                        currentState = State.LOG_INTO_ACCOUNT;
+                        break;
+                    case 0:
+                        System.out.println("Bye!");
+                        isRunning = false;
+                        break;
+                    default:
+                        System.out.println("Wrong number");
+
+
+                }
+                break;
+            case CREATE_AN_ACCOUNT:
+                currentState = State.SHOW_MENU;
+                Main.creteNewAccount();
+                break;
+            case LOG_INTO_ACCOUNT:
+                currentState = State.SHOW_MENU;
+                Main.login();
+                break;
+            case LOGGED_IN:
+                System.out.println("1. Balance\n2. Log out\n0. Exit");
+                int option = scanner.nextInt();
+                scanner.nextLine();
+                switch (option) {
+                    case 1:
+                        System.out.print("Balance: ");
+                        System.out.println(Main.data.get(1));
+                        break;
+                    case 2:
+                        Main.data.clear(); // remove the cache
+                        currentState = State.SHOW_MENU;
+                        System.out.println("You have successfully logged out!");
+                        break;
+                    case 0:
+                        isRunning = false;
+                        break;
+                    default:
+                        System.out.println("wrong number");
+                        break;
+                }
+        }
+    }
+
+
+}
